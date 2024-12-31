76dc2207ebaa421f18ccb876366adddd44508c65
mahendra <mahendradewangan195@gmail.com>
2024-12-31 11:00:31 +0530

    aftersignouturl:try2

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -1,7 +1,15 @@
 import Documents from '@/components/Documents'
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-export default function page() {
+export default async function page() {
+  const { userId } = await auth()
+
+  if (!userId) {
+    return redirect('/')
+  }
+
   return (
     <div className='w-full max-w-7xl mx-auto '>
         <h1 className='text-2xl p-5 font-extrabold pt-10'>
