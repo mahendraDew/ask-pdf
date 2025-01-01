f24d84f3523c2e620184ef6c45aa27ecaf9aa32d
mahendra <mahendradewangan195@gmail.com>
2025-01-01 16:43:31 +0530

    fix:signout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,12 @@ import { motion } from 'framer-motion'
 import { useState, useCallback, useRef, useEffect } from 'react'
 import Image from 'next/image'
 import { FileCheck2, FileUp, LogOut, Menu, X } from 'lucide-react'
-import { SignOutButton, useUser } from '@clerk/nextjs'
+import { SignOutButton, useClerk, useUser } from '@clerk/nextjs'
 import Link from 'next/link'
 import { Button } from './ui/button'
 import ModeToggle from './mode-toggle'
+import { redirect } from 'next/navigation'
+
 // import { redirect } from 'next/navigation'
 
 export function Header () {
@@ -16,6 +18,7 @@ export function Header () {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
   const dropdownRef = useRef<HTMLDivElement>(null)
   const shimmerRef = useRef<HTMLDivElement>(null)
+  const { signOut } = useClerk()
 
   const { user } = useUser()
   const toggleExpand = useCallback(() => {
@@ -184,19 +187,18 @@ export function Header () {
                       </div>
                     </div>
 
-                    <SignOutButton redirectUrl='/'>
                       <Button
                         variant={'default'}
                         className='mt-1 bg-transparent hover:bg-gray-300 text-gray-800  dark:text-gray-200 dark:hover:text-gray-900  px-4 py-2 text-sm w-full '
-                        // onClick={() => {
-                        //   redirect('/')
-                        // }}
+                        onClick={async () => {
+                          await signOut({ redirectUrl: '/' })
+                          redirect('/')
+                        }}
                       >
                         <span className='flex justify-left items-center gap-2'>
                           <LogOut /> Sign out
                         </span>
                       </Button>
-                    </SignOutButton>
                   </div>
                 </motion.div>
               )}
@@ -231,19 +233,20 @@ export function Header () {
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.5 }}
                   >
-                    <SignOutButton redirectUrl='/'>
+                    {/* <SignOutButton redirectUrl='/'> */}
                       <Button
                         variant={'default'}
                         className='mt-1 bg-transparent hover:bg-gray-300 text-gray-800  dark:text-gray-200 dark:hover:text-gray-900  px-4 py-2 text-sm w-full '
-                        // onClick={() => {
-                        //   redirect('/')
-                        // }}
+                        onClick={async () => {
+                          await signOut({ redirectUrl: '/' })
+                          redirect('/')
+                        }}
                       >
                         <span className='flex justify-left items-center gap-2'>
                           <LogOut /> Sign out
                         </span>
                       </Button>
-                    </SignOutButton>
+                    {/* </SignOutButton> */}
                   </motion.div>
                   <motion.div
                     initial={{ opacity: 0, y: -20 }}
